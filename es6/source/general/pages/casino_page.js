718afe19a14a61e3734335165d14da4bfa0a1f65
Georgi Stoychev <gstoychev@bsbulgaria.com>
2016-01-21 10:39:58 +0200

    Fix some selectors

--- es6/source/general/pages/casino_page.js
+++ es6/source/general/pages/casino_page.js
@@ -1,3 +1,32 @@
-/**
- * Created by georgi on 14.01.16.
- */
+var BasePage = require('../../../lib/base/base_page');
+
+class CasinoPage extends BasePage {
+
+    constructor() {
+        super();
+
+        //List of CSS selectors on the page
+        this.header = ".header_content h1";
+        this.userMenu = "#user_menu";
+        this.topWinners = "#top_winners";
+        this.gamesMenu = "#games_menu";
+        this.sligshowContent = "#slideshow_content";
+        this.gamesBottomMenu = "#inner_games_bottom_menu";
+        this.footerLogos = "img[alt~='footer_logos']";
+        this.gameTabPanels = ".game_tab_panels";
+        this.gameCategoryLinks = ".game_category_links";
+        this.footerLegal = "#footer_legal";
+    }
+    getHeader() { return this.header; }
+    getUserMenu() { return this.userMenu; }
+    getTopWinners() { return this.topWinners; }
+    getGamesMenu() { return this.gamesMenu; }
+    getSlideShowContent() { return this.sligshowContent; }
+    getGamesBottomMenu() { return this.gamesBottomMenu; }
+    getFooterLogos() { return this.footerLogos; }
+    getGameTabPanels() { return this.gameTabPanels; }
+    getGameCategoryLinks() { return this.gameCategoryLinks;}
+    getFooterLegal() { return this.footerLegal; }
+
+}
+module.exports = CasinoPage;
