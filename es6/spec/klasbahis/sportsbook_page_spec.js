f2aa044e7e404692b1fc1de34ace7413e10ee094
BBorisov1 <b.borisov1@abv.bg>
2016-02-02 15:30:53 +0200

    test

--- es6/spec/klasbahis/sportsbook_page_spec.js
+++ es6/spec/klasbahis/sportsbook_page_spec.js
@@ -6,24 +6,14 @@ page.start().suite('KlasBahis Sportsbook Page', function(parent) {
 		this.userName = find('#user_username');
 		this.password = find('#user_password');
 		this.submitButton = find('#logged_out_bar input[type="submit"]');
+		this.cashoutTab = find('#betslip .tabs a[data-link="open_mybets"]');
 		actions.setWindowSize(1920, 1080);
 	});
 
 	page.start().suite('Header Logo', function(child) {
 		child
 		.setCaptureElements(page.getHeader())
-		.capture('Logged out');
-	});
-
-	page.start().suite('User Menu', function(child) {
-		child
-		.setCaptureElements(page.getUserMenu())
-		.capture('Logged out')
-		.capture('Logged in', function(actions) {
-			actions.sendKeys(this.userName, 'supporttest');
-			actions.sendKeys(this.password, 'offsidebet10');
-			actions.click(this.submitButton);
-		});
+		.capture('plain');
 	});
 
 	page.start().suite('Navigation Menu', function(child) {
@@ -36,6 +26,14 @@ page.start().suite('KlasBahis Sportsbook Page', function(parent) {
 		child
 		.setCaptureElements(page.getBetslipWidget())
 		.capture('empty');
+	});
+
+	page.start().suite('Cashout widget', function(child) {
+		child
+		.setCaptureElements(page.getBetslipWidget())
+		.capture('Logged out', function(actions) {
+			actions.click(this.cashoutTab);
+		});
 	});
 
 	page.start().suite('Favourites Menu', function(child) {
@@ -53,18 +51,50 @@ page.start().suite('KlasBahis Sportsbook Page', function(parent) {
 	page.start().suite('Articles', function(child) {
 		child
 		.setCaptureElements(page.getArticlesWidget())
+		.ignoreElements('#article_timer', '.article_image_container')
 		.capture('plain');
 	});
 
 	page.start().suite('Live widget', function(child) {
 		child
 		.setCaptureElements(page.getLiveWidget())
+		.ignoreElements('#osg-events-region')
 		.capture('plain');
 	});
 
 	page.start().suite('Next Games', function(child) {
 		child
 		.setCaptureElements(page.getNextGamesWidget())
-		.ignoreElements('.event_name').capture('plain');
-	}); 
+		.ignoreElements('#coupon_next_games table')
+		.capture('plain');
+	});
+
+	page.start().suite('Footer', function(child) {
+		child
+		.setCaptureElements(page.getFooter())
+		.capture('plain');
+	});
+
+	page.start().suite('User Menu', function(child) {
+		child
+		.setCaptureElements(page.getUserMenu())
+		.capture('Logged out')
+		.ignoreElements('#user_balance span.user_value')
+		.capture('Logged in', function(actions) {
+			actions.sendKeys(this.userName, 'supporttest');
+			actions.sendKeys(this.password, 'offsidebet10');
+			actions.click(this.submitButton);
+		});
+	});
+
+	page.start().suite('Cashout widget - second state', function(child) {
+		child
+		.setCaptureElements(page.getBetslipWidget())
+		.ignoreElements('#betslip .badge', '#osg-my-bets-pending-items')
+		.capture('Logged in', function(actions) {
+			actions.click(this.cashoutTab);
+		});
+	});
+
+
 });
